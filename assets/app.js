89275aa6665821150faf591edf48972e8c18d857
Willian Pereira <willianbp88az@gmail.com>
2023-08-05 15:43:05 -0300

    updt

--- assets/app.js
+++ assets/app.js
@@ -16,10 +16,10 @@ particlesJS('particles-js',
 {
   "particles": {
     "number": {
-      "value": 1000,
+      "value": 50,
       "density": {
         "enable": true,
-        "value_area": 800
+        "value_area": 100
       }
     },
     "color": {
